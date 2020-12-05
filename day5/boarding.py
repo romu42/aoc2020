5936ade934faaac084f31e525da24e37b3dcd8f2
Roger Murray <roger.murray@internetstiftelsen>
2020-12-05 13:20:26 +0100

    refactored and cleaned up

--- day5/boarding.py
+++ day5/boarding.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python3
+# https://adventofcode.com/2020/day/5
 
 
 
-def split_it(seat: str, v, list) -> list:
-    pass
+def slice_it(seat: str, seats: list) -> list:
+    for slicer in seat:
+        if slicer == 'B' or slicer == 'R':
+            seats = (seats[int(len(seats)/2):])
+        else:
+            seats = (seats[:int(len(seats)/2)])
+    return int(seats[0])
+
 
 def get_seat(row: int, column: int) -> int:
     return row * 8 + column
+
 
 if __name__ == '__main__':
     seats = []
     with open('puzzle_input') as f:
         tickets = [ x for x in f ]
     for ticket in tickets:
-        rows = ticket[0:7]
-        columns = ticket[7:10]
-        r_seats = [x for x in range(0,128)]
-        c_seats = [x for x in range(0,8)]
-        
-        for slicer in rows:
-            if slicer == 'B':
-#                print(f'slicer is {slicer}')
-                r_seats = (r_seats[int(len(r_seats)/2):])
-#                print(r_seats)
-            else:
-#                print(f'slicer is {slicer}')
-                r_seats = (r_seats[:int(len(r_seats)/2)])
-#                print(r_seats)
-            row = int(r_seats[0])
-            
-        for slicer in columns:
-#            print(columns)
-#            print(c_seats)
-            if slicer == 'R':
-#                print(f'slicer is {slicer}')
-                c_seats = (c_seats[int(len(c_seats)/2):])
-#                print(c_seats)
-            else:
-#                print(f'slicer is {slicer}')
-                c_seats = (c_seats[:int(len(c_seats)/2)])
-#                print(c_seats)
-            column = int(c_seats[0])
-    
+        row = slice_it(ticket[0:7], [x for x in range(0,128)])
+        column = slice_it(ticket[7:10], [x for x in range(0,8)])
         seats.append(get_seat(row, column))
-    #for seat in seats:
-        #print(f'seat id: {seat}')
     print(f'The highest boarding card is: {max(seats)}')
+    
     seats.sort()
     for i, seat in enumerate(seats):
         try:
