08c5d18cd3c72af3260e3c076ef74357f204abd6
Roger Murray <roger.murray@internetstiftelsen>
2020-12-07 00:36:26 +0100

    second attempt, cleaned up the input andling and played around with list comprehenstion a bit more

--- day6/customs.py
+++ day6/customs.py
@@ -20,6 +20,7 @@ def clean_input(incoming) -> list:
                 line += f'{row.strip()}'
         return lines
 
+
 def get_answer_anyone(input) -> int:
     count = 0
     for line in input:
@@ -32,11 +33,30 @@ def get_answer_everyone(input) -> int:
     count = 0
     for line in input:
         group, answers = line.split('-')
-        count = count + len([[x, answers.count(x)] for x in set(answers) if answers.count(x) == int(group)])
+        count = count + len([x for x in set(answers) if answers.count(x) == int(group)])
     return count
+
+
+def get_answer_anyone2(incoming) -> int:
+    with open(incoming) as f:
+        lines = f.read().split("\n\n")
+        return sum([len(set(x.replace("\n", ""))) for x in lines])
+
+
+def get_answer_everyone2(incoming) -> int:
+    with open(incoming) as f:
+        lines = f.read().split("\n\n")
+        listan = ([[y for y in set(x) if x.count(y) == x.count("\n") + 1] for x in lines])
+        count = 0
+        for row in listan:
+            count = count + len(row)
+        return count
+    
+
 
 if __name__ == '__main__':
 
+    # fist attempt with some cludgy input clean up
     #input = clean_input('puzzle_input_test')
     input = clean_input('puzzle_input')
 
@@ -45,5 +65,9 @@ if __name__ == '__main__':
 
     answer = get_answer_everyone(input)
     print(f'the magic number for everyone is: {answer}')
-    
-         
+   
+
+    # Second attemt with some cleaner input handling thanks to https://github.com/bombsimon
+    print(get_answer_anyone2('puzzle_input'))
+    print(get_answer_everyone2('puzzle_input'))
+
